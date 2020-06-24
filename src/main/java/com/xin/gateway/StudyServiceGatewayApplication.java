afdc618d5df857f503fc714662494527cc4211b1
mfh <1193482868@qq.com>
2020-06-24 15:52:57 +0800

    first commit

---
+++ src/main/java/com/xin/gateway/StudyServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.xin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyServiceGatewayApplication.class, args);
+    }
+
+}
